8b5b97a34adaa04b5ad94dc472371b6bac3fe9b2
rosauda <rodrigo.sauda@hotmail.com>
2023-01-16 08:02:47 +0100

    Random cars generator

--- cars.py
+++ cars.py
@@ -1,23 +1,32 @@
-import random
 from turtle import Turtle
-from timmy import Timmy
-from scoreboard import Scoreboard
+import random
 
-colors = ["red", "blue", "green", "black", "yellow", "brown", "purple"]
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
 
 
-class Car(Turtle):
+class CarManager:
 
     def __init__(self):
-        super().__init__()
-        self.color(random.choice(colors))
-        self.shape("square")
-        self.shapesize(stretch_wid=2, stretch_len=1)
-        self.penup()
-        self.goto(450, 0)
-        self.setheading(90)
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
 
-    def car_move(self):
-        new_x = self.xcor() - 10
-        self.goto(new_x, self.ycor())
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
 
