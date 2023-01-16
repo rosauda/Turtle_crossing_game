361155c6715bc482667b4978d0f1bcba69d53ebb
rosauda <rodrigo.sauda@hotmail.com>
2023-01-16 07:39:53 +0100

    class Scoreboard

---
+++ scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 20, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-310, 260)
+        self.write(f"Level: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+
+    def timmy_crossed(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.clear()
+        self.goto(0, 0)
+        self.write("Game Over", move=False, align=ALIGNMENT, font=FONT)
